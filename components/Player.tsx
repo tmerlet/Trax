b0e7a45ecb0560144dfeba9a83d90b0a108e71a7
Toby Merlet <7794576+tmerlet@users.noreply.github.com>
2023-01-23 12:45:19 +0000

    shuffle, next song, seek bar controls

--- components/Player.tsx
+++ components/Player.tsx
@@ -21,14 +21,17 @@ import {
   MdOutlineRepeat,
 } from 'react-icons/md'
 import { useStore } from 'easy-peasy'
+import { formatTime } from '../lib/formatters'
 
 const Player = ({ songs, activeSong }) => {
   const [playing, setPlaying] = useState(true)
   const [index, setIndex] = useState(0)
   const [seek, setSeek] = useState(0.0)
+  const [seeking, setIsSeeking] = useState(false)
   const [repeat, setRepeat] = useState(false)
   const [shuffle, setShuffle] = useState(false)
   const [duration, setDuration] = useState(0.0)
+  const soundRef = useRef(null)
 
   const setPlayState = (value) => {
     setPlaying(value)
@@ -41,10 +44,58 @@ const Player = ({ songs, activeSong }) => {
     setRepeat((state) => !state)
   }
 
+  const prevSong = () => {
+    setIndex((state) => {
+      return state ? state - 1 : songs.length - 1
+    })
+  }
+
+  const nextSong = () => {
+    setIndex((state) => {
+      if (shuffle) {
+        const next = Math.floor(Math.random() * songs.length)
+
+        if (next === state) {
+          return nextSong()
+        }
+
+        return next
+      }
+
+      return state === songs.length - 1 ? 0 : state + 1
+    })
+  }
+
+  const onEnd = () => {
+    if (repeat) {
+      setSeek(0) // this is for the ui
+      soundRef.current.seek(0) // this is for reactHowler
+    } else {
+      nextSong()
+    }
+  }
+
+  const onLoad = () => {
+    const songDuration = soundRef.current.duration() // we have duration on state but safer to get from Howler, hopefully database matches
+    setDuration(songDuration)
+  }
+
+  const onSeek = (e) => {
+    setSeek(parseFloat(e[0])) // Unique to range component from Chakra, if you use Range from HTML5 event returns1 value
+    soundRef.current.seek(e[0])
+  }
+
   return (
     <Box>
       <Box>
-        <ReactHowler playing={playing} src={activeSong?.url} />
+        <ReactHowler
+          ref={soundRef}
+          playing={playing}
+          src={activeSong?.url}
+          onLoad={onLoad}
+          onEnd={onEnd}
+          volume={0.1}
+        />
       </Box>
       <Center color="gray.600">
         <ButtonGroup>
@@ -63,6 +114,7 @@ const Player = ({ songs, activeSong }) => {
             aria-label="previous"
             fontSize="24px"
             icon={<MdSkipPrevious />}
+            onClick={prevSong}
           />
           {playing ? (
             <IconButton
@@ -92,6 +144,7 @@ const Player = ({ songs, activeSong }) => {
             aria-label="next"
             fontSize="24px"
             icon={<MdSkipNext />}
+            onClick={nextSong}
           />
           <IconButton
             outline="none"
@@ -111,11 +164,16 @@ const Player = ({ songs, activeSong }) => {
           </Box>
           <Box width="80%">
             <RangeSlider
-              aria-label={['min', 'max']}
+              // eslint-disable-next-line jsx-a11y/aria-proptypes
+              aria-label={['min', 'max']} // weird aria label but required in this format for RangeSlider
               min={0}
-              max={300}
               step={0.1}
               id="player-range"
+              max={duration ? duration.toFixed(2) : 0}
+              onChange={onSeek}
+              value={[seek]} // always need to pass an array / RangeSlide specific
+              onChangeStart={() => setIsSeeking(true)}
+              onChangeEnd={() => setIsSeeking(true)}
             >
               <RangeSliderTrack bg="gray.800">
                 <RangeSliderFilledTrack bg="gray.600" />
@@ -124,7 +182,7 @@ const Player = ({ songs, activeSong }) => {
             </RangeSlider>
           </Box>
           <Box width="10%" textAlign="right">
-            <Text fontSize="xs">1:21</Text>
+            <Text fontSize="xs">{formatTime(duration)}</Text>
           </Box>
         </Flex>
       </Box>
